f371aa24b885619470c35548e453405b1b66a647
kuramubon8810 <nasa8810@gmail.com>
2018-10-31 16:06:24 +0900

    演習問題1-2～1-7の追加

---
+++ Ex1-3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+main()
+{
+	float fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;       /* 温度表の下限 */
+	upper = 300;     /* 上限 */
+	step = 20;       /* きざみ */
+
+	fahr = lower;
+	printf("Fahr Celsius\n");
+	while (fahr <= upper){
+		celsius = (6.0/9.0) * (fahr-32.0);
+		printf("%3.0f %6.1f\n", fahr, celsius);
+		fahr = fahr + step;
+	}
+}
---
+++ Ex1-4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+main()
+{
+	float fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;       /* 温度表の下限 */
+	upper = 300;     /* 上限 */
+	step = 20;       /* きざみ */
+
+	celsius = lower;
+	printf("Celsius Fahr\n");
+	while (celsius <= upper){
+		fahr = celsius / (5.0/9.0) + 32.0;
+		printf("%3.0f %6.1f\n", celsius, fahr);
+		celsius = celsius + step;
+	}
+}
